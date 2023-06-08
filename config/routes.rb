97b1f520e9d104abec2b5f055e84373852892591
Baskakovs <baskakovs@me.com>
2023-06-08 21:26:55 +0300

    v beta

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  
-  resources :locations
-  resources :news
-  resources :u_admins
-  resources :sessions
-  resources :admins
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  namespace :api do
+    resources :locations
+    resources :news
+    resources :u_admins
+    resources :sessions
+    resources :admins
+    # Routing logic: fallback requests for React Router.
+    # Leave this here to help deploy your app later!
+    get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
-  resource :admin, only: [:create]
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  post '/auth', to: 'sessions#is_logged_in?'
-  get '/news_landing', to: 'news#restricted_index'
-  resource :news
-  resource :locations
+    resource :admin, only: [:create]
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+    post '/auth', to: 'sessions#is_logged_in?'
+    get '/news_landing', to: 'news#restricted_index'
+    resource :news
+    resource :locations
+  end
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
